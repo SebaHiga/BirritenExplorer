1e3924df6ccd877e7fa0aba352847f26e351e1a0
Sebastian Higa <sebastian@Sebastians-MacBook-Air.local>
2022-10-02 01:28:50 -0300

    first commit

---
+++ app/src/main/java/com/higa/birritenexplorer/activities/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.higa.birritenexplorer.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.higa.birritenexplorer.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
