ac6dee5d265e5acc5e94e81b8e783c1de10ba87f
Sebastian Higa <sebastian@Sebastians-MacBook-Air.local>
2022-10-04 20:09:51 -0300

    updating room version to 2.4.0

--- app/src/main/java/com/higa/birritenexplorer/database/AppDatabase.kt
+++ app/src/main/java/com/higa/birritenexplorer/database/AppDatabase.kt
@@ -1,37 +1,37 @@
-//package com.higa.birritenexplorer.database
-//
-//import android.content.Context
-//import android.util.Log
-//import androidx.room.Database
-//import androidx.room.Room
-//import androidx.room.RoomDatabase
-//import com.higa.birritenexplorer.entities.Item
-//
-//
-//@Database(entities = [User::class], version = 1, exportSchema = false)
-//
-//public  abstract class appDatabase : ItemDatabase() {
-//
-//    abstract fun userDao(): userDao
-//
-//    companion object {
-//        var INSTANCE: appDatabase? = null
-//
-//        fun getAppDataBase(context: Context): appDatabase? {
-//            if (INSTANCE == null) {
-//                synchronized(appDatabase::class) {
-//                    INSTANCE = Room.databaseBuilder(
-//                        context.applicationContext,
-//                        appDatabase::class.java,
-//                        "myDB"
-//                    ).allowMainThreadQueries().build() // No es lo mas recomendable que se ejecute en el mainthread
-//                }
-//            }
-//            return INSTANCE
-//        }
-//
-//        fun destroyDataBase(){
-//            INSTANCE = null
-//        }
-//    }
-//}
+package com.higa.birritenexplorer.database
+
+import android.content.Context
+import android.util.Log
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.higa.birritenexplorer.entities.User
+
+
+@Database(entities = [User::class], version = 1, exportSchema = false)
+
+public  abstract class appDatabase : RoomDatabase() {
+
+    abstract fun userDao(): userDao
+
+    companion object {
+        var INSTANCE: appDatabase? = null
+
+        fun getAppDataBase(context: Context): appDatabase? {
+            if (INSTANCE == null) {
+                synchronized(appDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        appDatabase::class.java,
+                        "myDB"
+                    ).allowMainThreadQueries().build() // No es lo mas recomendable que se ejecute en el mainthread
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyDataBase(){
+            INSTANCE = null
+        }
+    }
+}
---
+++ app/src/main/java/com/higa/birritenexplorer/entities/User.kt
@@ -0,0 +1,29 @@
+package com.higa.birritenexplorer.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+class User (  id : Int, name : String , email : String) {
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    var id : Int
+
+    @ColumnInfo(name = "name")
+    var name : String
+
+    @ColumnInfo(name = "email")
+    var email : String
+
+
+
+    init {
+        this.id = id
+        this.name = name
+        this.email = email
+
+    }
+}
